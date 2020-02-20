47f0d90d9cb7b4009572b034eada1226077c6b5e
wskyeagle@163.com <wskyeagle@163.com>
2020-02-20 13:53:44 +0800

    first commit

---
+++ blog_project/urls.py
@@ -0,0 +1,41 @@
+#coding=utf-8
+"""blog_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include,url
+#from django.urls import path,re_path
+from django.contrib import admin
+from django.conf import settings
+from blog.upload import upload_image
+from django.views.static import serve
+from blog_project import settings
+#from django.conf.urls.uploads import uploads
+urlpatterns = [
+    # 映射到管理界面
+    url(r'^admin/', admin.site.urls),
+    #配置用于处理图片上传的url映射
+    url(r'^uploads/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),
+                        #django.views.static.serve专门用于处理静态文件
+
+                        #这里用到了settings中配置好的路径MEDIA_ROOT
+
+    #用于映射富文本编辑器的图片上传
+    url(r'^admin/upload/(?P<dir_name>[^/]+)$', upload_image),
+    #url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})    <img src = "/media/{{objects.pic}}">
+    #重构urls.py，把blog相关url放到blog之下
+    url(r'^',include('blog.urls')),
+
+]
+
